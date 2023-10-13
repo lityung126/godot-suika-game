31a54009442be087bf1a78f678224430441a400d
lityung126 <hf0932964541@gmail.com>
2023-10-13 18:10:55 +0800

    update build bat: exclude secret.ini when copy

--- tools/auto_build_win.bat
+++ tools/auto_build_win.bat
@@ -1,3 +1,3 @@
 cd /d %~dp0
 %godot% --headless -q ../suika-game/project.godot --export-release "Windows Desktop" "../build/win/suika.exe"
-robocopy ../suika-game/streaming_data ../build/win/streaming_data /E /XF *.import
+robocopy ../suika-game/streaming_data ../build/win/streaming_data /E /XF secret.ini *.import
